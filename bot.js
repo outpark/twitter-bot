e3198853579b8f39ef19a77dd55dbd6e5926c80a
Seongsoon Park <sp3005@nyu.edu>
2015-12-12 16:27:29 -0500

    init

---
+++ bot.js
@@ -0,0 +1,30 @@
+var Twit = require('twit');
+
+
+var T = new Twit({
+	consumer_key: 'zRYzXSnoO2qmNStvB0vSzk5q2',
+	consumer_secret: '4qES2oOjJgVPbQY4gpvez9ZXoXkRpwnVEG8A6SDXTZnLnMJORY',
+	access_token: '4333795461-WmVdr4o0oJMCZfBqi3Nn1wA4YsS2ukH6gwW3TOZ',
+	access_token_secret: 'wno3dBzkkIHgV3qJKtlkl9cNqiOQUNF4NqjOEMGWHGhFo'
+});
+
+
+function retweetRecent() {
+	T.get('search/tweets', {q: "NLP OR Natural Language Processing", result_type: "recent"},
+		function (err, data, response) {
+			if(!err) {
+				var retweetID = data.statuses[0].id_str;
+				T.post('statuses/retweet/' + retweetID, {}, function (err, response){
+					if (response){ console.log('Retweeted Tweet ID: ' + retweetID);}
+					if (err) {
+						console.log("retweet Error: ", err);
+					}
+				});
+			} else {
+				console.log("Search Error: ", err);
+			}
+		})
+}
+	
+	retweetRecent();
+	setInterval(retweetRecent, 1000000);
