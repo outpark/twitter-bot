e208f6c50e145bf64dc31f2fe1c157a6d462c5e4
Seongsoon Park <sp3005@nyu.edu>
2015-12-19 14:26:25 -0500

    fix

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'zRYzXSnoO2qmNStvB0vSzk5q2',
+	consumer_secret: '4qES2oOjJgVPbQY4gpvez9ZXoXkRpwnVEG8A6SDXTZnLnMJORY',
+	access_token: '4333795461-WmVdr4o0oJMCZfBqi3Nn1wA4YsS2ukH6gwW3TOZ',
+	access_token_secret: 'wno3dBzkkIHgV3qJKtlkl9cNqiOQUNF4NqjOEMGWHGhFo'
+}
